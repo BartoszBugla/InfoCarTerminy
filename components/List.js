ca8688a12d620ff88d7d42307bbf5e61353c7f1c
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2020-06-11 13:36:10 +0200

    sd

--- components/List.js
+++ components/List.js
@@ -1,19 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.ul`
-    font-size: 18px;
-`;
-const P = styled.p`
-    font-size: 27px;
-`;
-
 const List = ({ day, children }) => {
     return (
-        <Container>
-            <P>{day}</P>
+        <div>
+            <p>{day}</p>
             {children}
-        </Container>
+        </div>
     );
 };
 
