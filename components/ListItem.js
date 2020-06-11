08d4fde69c0a8021aa94077b42fd6f69e20b62fd
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2020-06-11 13:49:52 +0200

    bez styled

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.li`
-    list-style: none;
-`;
 
 const listItem = ({ children }) => {
-    return <Container>{children}</Container>;
+    return <div>{children}</div>;
 };
 
 export default listItem;
